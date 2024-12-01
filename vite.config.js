4b0086ce40144cd9e04caa6c126e088acd04bea3
susanna dontha <150016179+susanna995@users.noreply.github.com>
2024-12-01 09:59:52 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   "server.hmr.overlay":"false"
+// })
+
 export default defineConfig({
-  plugins: [react()],
-  "server.hmr.overlay":"false"
-})
+  base: '/StoryNexeus/',  // Set to the repository name (case-sensitive)
+});
