fcaf28fd92e8d4b0c329acbd242625997ecc370f
PreFS <susannadontha@gmail.com>
2024-12-21 07:40:53 +0530

    Added vite installation and updated package.json

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   "server.hmr.overlay":"false"
-// })
-
 export default defineConfig({
-  base: '/StoryNexeus/',  // Set to the repository name (case-sensitive)
-});
+  plugins: [react()],
+  "server.hmr.overlay":"false"
+})
+
+// export default defineConfig({
+//   base: '/StoryNexeus/',  // Set to the repository name (case-sensitive)
+// });
